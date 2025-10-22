65df67a36eff276998e94ce28979be433c38bb4a
M-Abubakar-Ansari <muhammadabubakarsiddiqueansari@gmail.com>
2025-10-23 00:03:35 +0500

    UI almost completed

---
+++ pages/Auth/PageLogin.py
@@ -0,0 +1,52 @@
+from UI import LabeledInput, Card, Center, Label, INIT_THEME, Button
+from extensions.formHandler import Variable, Form
+
+variables = [
+    Variable("identifier", ""),
+    Variable("password", ""),
+]
+form = Form(variables)
+
+inputs_and_labels = [
+    [
+        dict(
+            email = dict(
+                text = "Email/Password *",
+                clas = "text-lg font-bold",
+            )
+        ),
+        dict(
+            email = dict(
+                placeholder = "Your email or name...",
+                model = form.identifier,
+            )
+        ),
+    ],
+    [
+        dict(
+            password = dict(
+                text = "Password *",
+                clas = "text-lg font-bold",
+            )
+        ),
+        dict(
+            password = dict(
+                placeholder = "Password...",
+                password = True,
+                password_toggle_button = True,
+                clas="mb-2",
+                model = form.password,
+            ),
+        )
+    ]
+]
+
+async def create():
+    INIT_THEME()
+    with Center(clas="w-full h-full"):
+        with Card("max-w-md w-full h-fit flex flex-col"):
+            Label("Sign Up", "w-full border-b-2 text-2xl font-bold text-center")
+            for i in inputs_and_labels:
+                LabeledInput(*i)
+            Label("", "w-full border-1")
+            Button("Login", lambda:(), "w-full")
--- routes/RouteAuth.py
+++ routes/RouteAuth.py
@@ -1,10 +1,11 @@
 from .ROUTES import LOGIN, SIGNUP
 from nicegui.ui import page
 from pages.Auth.PageSignUp import create as create_signup
+from pages.Auth.PageLogin import create as create_login
 
 @page(LOGIN)
-def render_login():
-    pass 
+async def render_login():
+    await create_login()
 
 @page(SIGNUP)
 async def render_signup():
